0b10406782f6b00d96c466e9bbdfc2ab342c7575
Bryce Frazier and Ron Craig <bryce@>
2018-04-16 16:46:50 -0700

    add links to animals details, buggy add new species

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,3 +1,28 @@
 class AnimalsController < ApplicationController
 
-end   
+  def index
+      @animals = Animal.all
+      render :index
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+    render :show
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def animal_params
+      params.require(:animal).permit(:species)
+    end
+
+
+end
